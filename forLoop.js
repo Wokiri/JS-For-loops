0d78a76e182f543269da736194af29d66a4a636d
Joe Oketch <wokirijoe@gmail.com>
2020-09-04 17:27:16 +0300

    Log the first 10 Prime numbers
    This example entailed using a nested for-loop

--- forLoop.js
+++ forLoop.js
@@ -1,9 +1,49 @@
-for (let numbers = 5; numbers <= 12; numbers += 1) {
-    console.log(numbers)
-}
+// Log the first 10 prime numbers
 
-// Numbers += 1 may also be written as numbers ++
-// They both mean an increment of 1 in every iteration
-// An increment of 2 would be numbers += 2
+// A prime number is any number that is divisible ONLY by itself and 1.
 
-// The variable 'numbers' is accessible within the loop block
+
+let primeNumbers = []
+let neededNumber = 10 //The number of prime numbers needed
+let indexStorage = 2 // helps to resolve a case where a array would contain an empty space
+let thePrimeNumbers = ''
+
+/* We need this first loop to access every item in counting from 0*/
+loopAllNumbers:
+    for (let prime = 0;; prime++) {
+
+        if (prime <= 1) {
+            continue loopAllNumbers
+        }
+
+        if (prime === 2) {
+            primeNumbers[indexStorage - 2] = prime
+            thePrimeNumbers += `${prime}, `
+            continue loopAllNumbers
+        }
+
+        /* We need this second loop to check every "primes" UNTIL when the array is full*/
+        loopTestingWithDivisors:
+            for (let items = prime - 1; items >= 2; items--) {
+                let isMultiple = prime % items === 0
+
+                if (isMultiple) {
+                    indexStorage++
+                    break loopTestingWithDivisors
+                } else {
+                    if (items === 2) {
+                        primeNumbers[prime - indexStorage] = prime
+                        thePrimeNumbers += `${prime}, `
+                        break loopTestingWithDivisors
+                    }
+                    continue loopTestingWithDivisors
+                }
+            }
+
+        if (primeNumbers.length === neededNumber) {
+            break
+        }
+    }
+
+console.log(thePrimeNumbers)
+console.log(primeNumbers)
